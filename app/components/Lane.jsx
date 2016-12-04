a8bac776fab9794956b1c9cc9876ae5484186765
Konrad Kale <konrad.kale@icloud.com>
2016-12-04 17:43:25 -0500

    complete

--- app/components/Lane.jsx
+++ app/components/Lane.jsx
@@ -1,20 +1,20 @@
-import React from 'react';
 
+import React from 'react';
+import {compose} from 'redux';
+import {DropTarget} from 'react-dnd';
+import ItemTypes from '../constants/itemTypes';
 import connect from '../libs/connect';
 import NoteActions from '../actions/NoteActions';
 import LaneActions from '../actions/LaneActions';
 import Notes from './Notes';
 import LaneHeader from './LaneHeader';
 
-
-
 const Lane = ({
-  lane, notes, LaneActions, NoteActions, ...props
+  connectDropTarget, lane, notes, LaneActions, NoteActions, ...props
 }) => {
   const editNote = (id, task) => {
     NoteActions.update({id, task, editing: false});
   };
-
   const deleteNote = (noteId, e) => {
     e.stopPropagation();
 
@@ -27,7 +27,8 @@ const Lane = ({
   const activateNoteEdit = id => {
     NoteActions.update({id, editing: true});
   };
-  return (
+
+  return connectDropTarget(
     <div {...props}>
       <LaneHeader lane={lane} />
       <Notes
@@ -40,18 +41,47 @@ const Lane = ({
 };
 
 function selectNotesByIds(allNotes, noteIds = []) {
+  // `reduce` is a powerful method that allows us to
+  // fold data. You can implement `filter` and `map`
+  // through it. Here we are using it to concatenate
+  // notes matching to the ids.
   return noteIds.reduce((notes, id) =>
-  notes.concat(
-    allNotes.filter(note => note.id === id)
-  )
+    // Concatenate possible matching ids to the result
+    notes.concat(
+      allNotes.filter(note => note.id === id)
+    )
   , []);
 }
 
-export default connect (
-  ({notes}) => ({
+const noteTarget = {
+  hover(targetProps, monitor) {
+    const sourceProps = monitor.getItem();
+    const sourceId = sourceProps.id;
+
+    // If the target lane doesn't have notes,
+    // attach the note to it.
+    //
+    // `attachToLane` performs necessarly
+    // cleanup by default and it guarantees
+    // a note can belong only to a single lane
+    // at a time.
+    if(!targetProps.lane.notes.length) {
+      LaneActions.attachToLane({
+        laneId: targetProps.lane.id,
+        noteId: sourceId
+      });
+    }
+  }
+};
+
+export default compose(
+  DropTarget(ItemTypes.NOTE, noteTarget, connect => ({
+    connectDropTarget: connect.dropTarget()
+  })),
+  connect(({notes}) => ({
     notes
   }), {
     NoteActions,
     LaneActions
-  }
-) (Lane)
+  })
+)(Lane)
